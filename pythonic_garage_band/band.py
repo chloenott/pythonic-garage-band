80bd19e574b1fabad34431f4ec98df401f1e757d
Chloe Nott <chloe@nott.email>
2021-12-02 19:18:01 -0800

    Added dunder repr method to Drummer. test_drummer_repr test passes

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -24,4 +24,7 @@ class Drummer:
     def __str__(self):
         return f"My name is {self.name} and I play drums"
 
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
 
