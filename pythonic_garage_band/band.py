c52c149d23ea63bcc9b4f395e6066956698701d3
Chloe Nott <chloe@nott.email>
2021-12-02 19:11:43 -0800

    Updated Guitarist class to have dunder str method. test_guitarist_str method passes.

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -5,7 +5,11 @@ class Musician:
     pass
 
 class Guitarist:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
 
 class Bassist:
     pass
